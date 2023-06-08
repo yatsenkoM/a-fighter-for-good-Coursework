dfd114e43126a2a69b1d934f6a842435abbef30c
Maksym Yatsenko <m.yatsenko.im22@kpi.ua>
2023-06-08 12:25:33 +0300

    replacing the function "PausePosition" with using of class

--- scripts/positionPause.js
+++ scripts/positionPause.js
@@ -1,33 +1,33 @@
-function PausePosition() {
+class PausePosition {
+    constructor() {
+    }
+    draw(play) {
+        ctx.clearRect(0, 0, play.width, play.height);
+        ctx.font = "40px Montserrat";
+        ctx.fillStyle = '#ffffff';
+        ctx.textAlign = "center";
+        ctx.fillText("Paused", play.width / 2, play.height / 2 - 300);
+
+        ctx.fillStyle = '#D7DF01';
+        ctx.font = "36px Montserrat";
+        ctx.fillText("P: back to the current game", play.width / 2, play.height / 2 - 250);
+        ctx.fillText("ESC: quit the current game", play.width / 2, play.height / 2 - 210);
+
+        ctx.font = "40px Montserrat";
+        ctx.fillStyle = '#ffffff';
+        ctx.fillText("Game controls reminder", play.width / 2, play.height / 2 - 120);
+        ctx.fillStyle = '#D7DF01';
+        ctx.font = "36px Montserrat";
+        ctx.fillText("Left Arrow : Move Left", play.width / 2, play.height / 2 - 70);
+        ctx.fillText("Right Arrow : Move Right", play.width / 2, play.height / 2 - 30);
+        ctx.fillText("Space : Fire", play.width / 2, play.height / 2 + 10);
+    }
+    keyDown(play, keyboardCode) {
+        if (keyboardCode === 80) {
+            play.popPosition();
+        }
+        if (keyboardCode === 27) {
+            play.pushPosition(new GameOverPosition());
+        }
+    }
 }
-
-PausePosition.prototype.draw = function(play) {
-    ctx.clearRect(0, 0, play.width, play.height);
-    ctx.font="40px Montserrat";
-    ctx.fillStyle = '#ffffff';
-    ctx.textAlign="center";
-    ctx.fillText("Paused", play.width / 2, play.height/2 - 300);
-
-    ctx.fillStyle = '#D7DF01';
-	ctx.font="36px Montserrat";
-	ctx.fillText("P: back to the current game", play.width / 2, play.height/2 - 250 );
-    ctx.fillText("ESC: quit the current game", play.width / 2, play.height/2 - 210);
-    
-    ctx.font="40px Montserrat";
-    ctx.fillStyle = '#ffffff';
-	ctx.fillText("Game controls reminder", play.width / 2, play.height/2 - 120);
-	ctx.fillStyle = '#D7DF01';
-	ctx.font="36px Montserrat";
-	ctx.fillText("Left Arrow : Move Left", play.width / 2, play.height/2 - 70);
-	ctx.fillText("Right Arrow : Move Right", play.width / 2, play.height/2 - 30);
-	ctx.fillText("Space : Fire", play.width / 2, play.height/2 + 10);
-};
-
-PausePosition.prototype.keyDown = function(play, keyboardCode) {
-    if(keyboardCode === 80) {
-        play.popPosition();
-    }
-    if(keyboardCode === 27) {
-        play.pushPosition(new GameOverPosition());
-    }
-};
