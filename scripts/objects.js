e317f4dc9f82b8d1ab5decbd0a3953a3d8938aad
Maksym Yatsenko <m.yatsenko.im22@kpi.ua>
2023-06-08 12:18:42 +0300

    replacing the function "Objects" with using of class

--- scripts/objects.js
+++ scripts/objects.js
@@ -1,37 +1,35 @@
-function Objects() {
+class Objects {
+	constructor() {
+	}
+	fighter(x, y, imageOfFighter) {
+		this.x = x;
+		this.y = y;
+		this.width = 34;
+		this.height = 28;
+		this.imageOfFighter = imageOfFighter;
+		this.imageOfFighter.src = "images/fighter.png";
+		return this;
+	}
+	bullet(x, y) {
+		this.x = x;
+		this.y = y;
+		return this;
+	}
+	enemy(x, y, line, column, imageOfEnemy, level) {
+		this.x = x;
+		this.y = y;
+		this.line = line;
+		this.column = column;
+		this.width = 32;
+		this.height = 24;
+		this.imageOfEnemy = imageOfEnemy;
+		this.level = level;
+		this.imageOfEnemy.src = (this.level % 2 === 0) ? "images/enemy2.png" : "images/enemy1.png";
+		return this;
+	}
+	bomb(x, y) {
+		this.x = x;
+		this.y = y;
+		return this;
+	}
 }
-
-Objects.prototype.fighter = function (x, y, imageOfFighter) {
-	this.x = x;
-	this.y = y;
-	this.width = 34;
-	this.height = 28;
-	this.imageOfFighter = imageOfFighter;
-	this.imageOfFighter.src = "images/fighter.png";
-	return this;
-};
-
-Objects.prototype.bullet = function(x, y) {
-    this.x = x;
-    this.y = y;
-	return this;
-};
-
-Objects.prototype.enemy = function(x, y, line, column, imageOfEnemy, level) {
-    this.x = x;
-    this.y = y;
-    this.line = line;
-    this.column = column;
-    this.width = 32;  
-    this.height = 24;  
-	this.imageOfEnemy = imageOfEnemy;
-	this.level = level;
-	this.imageOfEnemy.src = (this.level % 2 === 0) ? "images/enemy2.png" : "images/enemy1.png";
-	return this;	
-};
-
-Objects.prototype.bomb = function(x, y) {
-    this.x = x;
-    this.y = y;
-	return this;
-};
