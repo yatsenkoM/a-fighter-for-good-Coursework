25a848e57e25d19fadd80b0ce6235d78d62a98ce
Maksym Yatsenko <m.yatsenko.im22@kpi.ua>
2023-06-08 12:27:22 +0300

    replacing the function "TransferPosition" with using of class

--- scripts/positionTransfer.js
+++ scripts/positionTransfer.js
@@ -1,21 +1,21 @@
-function TransferPosition(level) {
-    this.level = level;
-    this.fontSize = 140;
-    this.fontColor = 255;
-}
-
-TransferPosition.prototype.update = function (play) {
-    this.fontSize -= 1;
-    this.fontColor -= 1.5;
-    if (this.fontSize < 1) {
-        play.goToPosition(new InGamePosition(play.setting, this.level));
+class TransferPosition {
+    constructor(level) {
+        this.level = level;
+        this.fontSize = 140;
+        this.fontColor = 255;
     }
-};
-
-TransferPosition.prototype.draw = function (play) {
-    ctx.clearRect(0, 0, play.width, play.height);
-    ctx.font = this.fontSize + "px Montserrat";
-    ctx.textAlign = "center";
-    ctx.fillStyle = "rgba(255, " + this.fontColor + ", " + this.fontColor + ", 1)";
-    ctx.fillText("Get ready for level " + this.level, play.width / 2, play.height / 2);
-};
+    update(play) {
+        this.fontSize -= 1;
+        this.fontColor -= 1.5;
+        if (this.fontSize < 1) {
+            play.goToPosition(new InGamePosition(play.setting, this.level));
+        }
+    }
+    draw(play) {
+        ctx.clearRect(0, 0, play.width, play.height);
+        ctx.font = this.fontSize + "px Montserrat";
+        ctx.textAlign = "center";
+        ctx.fillStyle = "rgba(255, " + this.fontColor + ", " + this.fontColor + ", 1)";
+        ctx.fillText("Get ready for level " + this.level, play.width / 2, play.height / 2);
+    }
+}
