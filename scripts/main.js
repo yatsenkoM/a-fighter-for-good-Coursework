a192f2905887e37eb8b67328de7f442452b851d0
Maksym Yatsenko <m.yatsenko.im22@kpi.ua>
2023-06-09 16:47:32 +0300

    using template literals

--- scripts/main.js
+++ scripts/main.js
@@ -7,8 +7,8 @@ function resize() {
   const height = window.innerHeight - 20;
   const ratio = canvas.width / canvas.height;
   const width = height * ratio;
-  canvas.style.width = width + 'px';
-  canvas.style.height = height + 'px';
+  canvas.style.width = `${width}px`;
+  canvas.style.height = `${height}px`;
 }
 window.addEventListener('load', resize, false);
 
